18558f9e0eac27cb4291e5d12276fd1c61390388
Mohamed Ayacha <ayachamed1919@gmail.com>
2025-04-30 21:10:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myisamm/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.myisamm;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/myisamm/SplashActivity.java
@@ -0,0 +1,6 @@
+package com.example.myisamm;
+
+import android.app.Activity;
+
+public class SplashActivity extends Activity {
+}
